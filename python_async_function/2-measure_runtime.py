e1603e270838196165270785e896466aa75a4481
Gabr1el20 <6327@holbertonstudents.com>
2023-08-28 08:26:03 -0700

    measure_time was never an async routine...

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -5,10 +5,10 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int = 0, max_delay: int = 10) -> float:
+def measure_time(n: int = 0, max_delay: int = 10) -> float:
     """takes two int as args, measures the total execution time
     for wait_n, and returns total_time / n"""
     start = time.perf_counter()
     execution = asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter - start
+    end = time.perf_counter() - start
     return end / n
