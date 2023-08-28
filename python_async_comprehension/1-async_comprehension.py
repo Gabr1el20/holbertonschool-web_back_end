7751968599a044180a6637520b1363b7cae4a4ad
Gabr1el20 <6327@holbertonstudents.com>
2023-08-28 09:55:16 -0700

    Done with async comprehension :)

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 "task 1: Async comprehension"
-from typing import Generator
+from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[list, None, None]:
+async def async_comprehension() -> List[float]:
     """this coroutine will collect 10 random numbers
     using an async comprehension over async_generator, then
     will return all those numbers."""
-    list_of_yields = []
-    async for i in async_generator():
-        list_of_yields.append(i)
+    list_of_yields = [i async for i in async_generator()]
     return list_of_yields
