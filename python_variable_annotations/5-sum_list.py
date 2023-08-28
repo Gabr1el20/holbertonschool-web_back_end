99711e82a8829fe985e774d41793d602a719ebea
Gabr1el20 <6327@holbertonstudents.com>
2023-08-28 06:15:25 -0700

    Added documentation of sum_list() function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,6 +4,8 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """sum_list function that returns
+    the sum of all members of a float list"""
     return_sum: float = 0
     for member in input_list:
         return_sum += member
