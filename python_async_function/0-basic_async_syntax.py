40939b5cc1b64d6ba1797d4ad338a34fe0042459
Gabr1el20 <6327@holbertonstudents.com>
2023-08-28 07:26:29 -0700

    Pycode fixing

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,8 @@
 from random import uniform
 import asyncio
 
-async def wait_random(max_delay: int=10) -> float:
+
+async def wait_random(max_delay: int = 10) -> float:
     """takes an int arg, waits for a random delay
     that has a max of max_delay value and eventually
     returns it"""
