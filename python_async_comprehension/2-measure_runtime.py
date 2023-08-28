d895e412b11e0baffd51d22a7a5b276b8cca88f1
Gabr1el20 <6327@holbertonstudents.com>
2023-08-28 10:11:02 -0700

    Fixed runtime return

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,5 +8,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """will execute async_comprehension four times
     in parallel using asyncio.gather"""
+    start = time.perf_counter()
     await asyncio.gather(async_comprehension())
-    return time.perf_counter()
+    return time.perf_counter() - start
