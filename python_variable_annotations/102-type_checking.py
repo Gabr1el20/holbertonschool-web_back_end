22db1920b77df57611bee8f592dc8b69f67b8f56
Gabr1el20 <6327@holbertonstudents.com>
2023-08-29 06:03:33 -0700

    Task 102: documented

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"Task 102: fixing type annotation of a function"
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    "zoom_array() function :)"
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
