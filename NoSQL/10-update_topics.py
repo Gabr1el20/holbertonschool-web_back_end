d35601ca2b09364b1e6269465443ae516bd14189
Gabr1el20 <6327@holbertonstudents.com>
2023-09-04 07:55:50 -0700

    Task 10: Changed one() to many()

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     based on the name"""
     query = {"name": name}
     update = {"$set": {"topics": topics}}
-    mongo_collection.update_one(query, update)
+    mongo_collection.update_many(query, update)
