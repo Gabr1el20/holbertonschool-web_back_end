558b377a084c42228bf8051d44c5f3c34eb6b103
Gabr1el20 <6327@holbertonstudents.com>
2023-09-15 09:34:34 -0700

    Task 4:pricing

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,13 +1,31 @@
-import Currency from './3-currency';
+import Currency from './3-currency.js';
 
 export default class Pricing {
-  constructor(amount = 0, currency = '') {
+  constructor(amount = 0, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
   set amount(value) {
     this._amount = value;
+  }
+
+  displayFullPrice() {
+    const code = (this.currency._code);
+    const name = (this.currency._name);
+    const money = `${this.amount} ${name} ${code})`;
+
+    return money;
+  }
+
+  static convertPrice(amount = 0, conversionRate = 0) {
+    if (typeof (conversionRate) !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    if (typeof (amount) !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    return (amount * conversionRate);
   }
 
   set currency(value) {
