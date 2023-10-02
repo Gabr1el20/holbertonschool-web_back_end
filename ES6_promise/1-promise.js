ef51c2b747455dd3bc07bd230db766348d8b1cea
Gabr1el20 <6327@holbertonstudents.com>
2023-10-02 09:23:49 -0700

    Task 1: fixed with promise returned

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,8 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) {
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }
-  return new Error('The fake API is not working currently');
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'success' });
+    }
+    reject(new Error('The fake API is not working currently'));
+  });
 }
