bf61df74c23fe65394f761d0453cbcde0c4d1012
Gabr1el20 <6327@holbertonstudents.com>
2023-10-03 09:40:46 -0700

    Added dataView

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
   }
   const buffer = new ArrayBuffer(length);
   const arr = new Int8Array(buffer);
-
+  const vue = new DataView(buffer);
   arr[position] = value;
-  return arr;
+  return vue;
 }
