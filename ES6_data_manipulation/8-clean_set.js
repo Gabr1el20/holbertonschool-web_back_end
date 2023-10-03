fa813f594e12264bb0ac24659a8d916a8cd27de8
Gabr1el20 <6327@holbertonstudents.com>
2023-10-03 14:18:02 -0700

    Fixing in process for task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(aSet, startString) {
-  if (startString === '') return '';
+  if (startString === '' || !(startString instanceof String) || !startString) return '';
   const filteredValues = Array.from(aSet)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length));
