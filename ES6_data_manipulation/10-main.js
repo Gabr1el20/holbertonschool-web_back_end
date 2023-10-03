7ec2bcb4046755af26f21e9b21988027737c158f
Gabr1el20 <6327@holbertonstudents.com>
2023-10-03 14:09:51 -0700

    Task 10

---
+++ ES6_data_manipulation/10-main.js
@@ -0,0 +1,10 @@
+import updateUniqueItems from './10-update_uniq_items';
+import groceriesList from './9-groceries_list';
+
+const map = groceriesList();
+const str = '';
+console.log(map);
+
+updateUniqueItems(map);
+updateUniqueItems(str);
+console.log(map);
